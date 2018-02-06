699b794ffa3490d610af214176ff850525aebed6
Patrick Biffle <pbiffle@gmail.com>
2018-02-06 13:24:40 -0700

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const queries = require('./queries')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	next()
+})
 
 app.get('/users', (request, response) => {
 	queries
--- knexfile.js
+++ knexfile.js
@@ -7,8 +7,6 @@ module.exports = {
 	},
 	production: {
 		client: 'pg',
-		// connection:
-		// 	'postgres://qtcbijksljenim:c82606ba29f7955e45761d6a53f85596ffdffa2cd6db7757d0fa1be9be164b6a@ec2-54-197-253-122.compute-1.amazonaws.com:5432/dakf5q2tervvjh'
 		connection: process.env.DATABASE_URL
 	}
 }
